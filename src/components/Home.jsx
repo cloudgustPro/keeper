3afbfd114e68df57b247e8f03b860f32646e717c
cloudgustPro <cloudgust.pro@gmail.com>
2023-08-03 11:32:07 +0300

    toggle

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,19 +6,23 @@ const Home = () => {
                                         {id: 2, name: "start patient management system", completed: false},
                                         {id: 3, name: "connect github account to vscode", completed: true}
                                     ])
+    const[show, setShow] = useState(true)
     
-    const handleDelete = () =>{
+    const handleDelete = (id) =>{
+        setTasks(tasks.filter(task =>task.id != id))
 
     }
   return (
     <div className='mt-4 mx-8 w-auto'>
         <ul>
-            {tasks.map((task)=>(
+            <button className='ml-80 border-2 shadow rounded-md bg-blue-800 text-white p-1' onClick={()=>setShow(!show)}>Toggle</button>
+            {show && tasks.map((task)=>(
                 <li key={task.id} className='border shadow-sm rounded-md mb-4 p-2'>
                     <span>{task.id} - {task.name}</span>
-                    <button onClick={handleDelete} className='flex justify-end bg-slate-400 border rounded-md p-1'>Delete</button>
+                    <button onClick={()=>handleDelete(task.id)} className='flex justify-end bg-slate-400 border rounded-md p-1'>Delete</button>
                 </li>
             ))}
+            
         </ul>
     </div>
   )
